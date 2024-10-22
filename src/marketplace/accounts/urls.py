b8f68aaaa486df943e8f5a646eb7abca143747b6
Daniel Plazas <danieljpc19@gmail.com>
2024-10-21 22:27:38 -0500

    Eliminar todo del seguimiento de Git

--- src/marketplace/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import CustomLoginView
-
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-
-]
-
--- src/marketplace/perfil/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('perfilesFreelancer/<int:id>/', views.perfilesFreelancer, name='perfilFreelancer'),
-    path('perfilesCliente/<int:id>/', views.perfilesCliente, name='perfilesCliente'),
-]
