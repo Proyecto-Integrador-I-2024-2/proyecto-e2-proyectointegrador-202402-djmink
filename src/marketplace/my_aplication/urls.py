056abaa2a7c4d858bea52c52bb9b0a7aaa4be0b6
Julianmotta07 <mottajulian47@gmail.com>
2024-11-03 22:58:32 -0500

    feat: getting ready for merge

--- src/marketplace/my_aplication/urls.py
+++ src/marketplace/my_aplication/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('users/', views.users, name='users'),
-    path('register/', views.register, name='register'),
-    path('complete/<int:profile_id>/', views.complete, name='complete'),
     path('home/', views.home, name='home'),
     path('', views.home, name='home'),
 ]
