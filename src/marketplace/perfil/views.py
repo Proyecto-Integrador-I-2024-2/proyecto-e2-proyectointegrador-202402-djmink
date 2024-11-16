2128fd9b0cb5b706c23540f32b91ba0d2d4f003b
Julianmotta07 <mottajulian47@gmail.com>
2024-11-16 01:58:09 -0500

    feat: simple payments simulation implementation and fix at add project functionality

--- src/marketplace/perfil/views.py
+++ src/marketplace/perfil/views.py
@@ -661,7 +661,7 @@ def freelancerProfile(request, id, idclient):
         'profile_image': profile_image,
         'client': client,
         'profile_url': profile_url,
-        'home_url': reverse('mainClient', args=[p.id]),
+        'home_url': reverse('mainCliente', args=[p.id]),
         'at_client_page': True,
         'can_rate': True,
         'show_edit_link': False})
@@ -777,7 +777,13 @@ def manageProject(request, id, id_project):
             milestone = application.milestone
             if milestone.freelancer is None:
                 application.state = 'Approved'
-                milestone.freelancer = get_object_or_404(Freelancer, id=application.freelancer.id)
+                freelancer = get_object_or_404(Freelancer, id=application.freelancer.id)
+                milestone.freelancer = freelancer
+
+                for task in milestone.tasks.all():
+                    task.freelancer = freelancer
+                    task.save()
+
                 application.save()
                 milestone.save()
                 #Notificar al freelancer
@@ -803,6 +809,20 @@ def manageProject(request, id, id_project):
             #Notificar al freelancer
             messages.success(request, "Comentario enviado al freelancer exitosamente.")
         
+        elif action == "make_payment":
+            milestone_id = request.POST.get("milestone")
+            milestone = get_object_or_404(Milestone, id=milestone_id)
+            milestone.paid = True
+            milestone.save()
+
+            currency = request.POST.get("currency")
+            amount = request.POST.get("amount")
+            freelancer_id = request.POST.get("freelancer")
+            freelancer = get_object_or_404(Freelancer, id=freelancer_id)
+            freelancer_name = freelancer.name
+            #Notificar al freelancer
+            messages.success(request, f"{amount} {currency} enviados a {freelancer_name} exitosamente.")
+
         return redirect('manageProject', id, id_project)
 
     #progress bar
@@ -823,6 +843,7 @@ def manageProject(request, id, id_project):
         'milestones': [
             {
                 'id': milestone.id,
+                'paid': milestone.paid,
                 'name': milestone.name,
                 'progress1': milestone.progress,
                 'start_date': milestone.start_date.strftime('%Y-%m-%d'),
@@ -835,6 +856,7 @@ def manageProject(request, id, id_project):
                     for task in milestone.tasks.all()
                 ],
                 'freelancer': {
+                    'id': milestone.freelancer.id,
                     'name': milestone.freelancer.name if milestone.freelancer else None,
                     'profile_picture': milestone.freelancer.image.url if milestone.freelancer and milestone.freelancer.image else None,
                 } if milestone.freelancer else None,
@@ -895,7 +917,8 @@ def projectWorkspace(request, id, id_project):
                     if task_state:
                         task.state = task_state
                         task.save()
-
+                
+                milestone.calculate_progress()
                 messages.success(request, f"Estado de las tareas del milestone '{milestone.name}' actualizado exitosamente.")
 
         elif action == "send_assignment":
@@ -1013,3 +1036,6 @@ def deleteDisableClient(request, id=id):
 
 def calendar(request):
     return render(request, 'perfil/calendar.html')
+
+
+
